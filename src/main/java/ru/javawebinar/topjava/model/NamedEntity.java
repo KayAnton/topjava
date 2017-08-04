518892ee80aec5420960943809e769328d847cd3
Anton Kai <akai@data-tech.com>
2017-08-04 13:51:16 +0300

    Add JPA

--- src/main/java/ru/javawebinar/topjava/model/NamedEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/NamedEntity.java
@@ -1,7 +1,16 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
 public class NamedEntity extends BaseEntity {
 
+    @NotBlank
+    @Column(name = "name", nullable = false)
     protected String name;
 
     public NamedEntity() {
